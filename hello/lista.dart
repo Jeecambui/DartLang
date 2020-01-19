f429c8684d113b30f3dcd8defe365b10e7c5b606
unknown <jeferson_cambui@hotmail.com>
2020-01-19 20:44:08 -0300

    aula 3 Lista

--- hello/lista.dart
+++ hello/lista.dart
@@ -1,6 +1,6 @@
 void main() {
-  List listaPordutos = [
-1, 2, 3, 4, 5, 6, 7, 8, 9, 10
-  ];
-  }
+  List listaProdutos = [
+1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 'Arroz', 1.50, true
+];
+  print(listaProdutos);
 }
