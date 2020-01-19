bc1d2bad900b3c4df4e661cc144c28f65f00cd58
unknown <jeferson_cambui@hotmail.com>
2020-01-19 19:11:34 -0300

    variaveis

---
+++ hello/variaveis.dart
@@ -0,0 +1,10 @@
+void main(){
+  String nome = "Jeferson Cambui";
+  print(nome);
+  int idade = 24;
+  print(idade);
+  double tamanho = 1.83;
+  print(tamanho);
+  bool jefersonDev = false;
+  print(jefersonDev);
+}
