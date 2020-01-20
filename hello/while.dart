249f185ba59a937dca5bbc1aee2ce17aaad33c5c
unknown <jeferson_cambui@hotmail.com>
2020-01-19 23:05:54 -0300

    DO While

--- hello/while.dart
+++ hello/while.dart
@@ -1,3 +1,11 @@
 void main (){
-  
+  int idade = 10;
+  while (idade <18){
+    print(idade.toString() + ' é menor que 18 anos');
+    idade ++;
+  }
+
+  do {
+    print('Teste DO While');
+  } while(false);
 }
