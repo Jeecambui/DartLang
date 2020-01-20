236d4ddc886799f0820e5bb76173b7de1a572c99
unknown <jeferson_cambui@hotmail.com>
2020-01-19 21:32:54 -0300

    aula 8 If e Else

--- hello/controles.dart
+++ hello/controles.dart
@@ -1,6 +1,12 @@
 void main(){
-  if (true){
-    print('o if sendo executado eu acho');
+  var idade = 17;
+  if (idade >= 18){
+    print('Antonieto pode ter um cartão de credito!');
   }
-  print('finalizou');
+  else{
+    print('Antonieto vai ter o cartão HOJE !');
+  }
 }
+
+
+ // var sexo = 'Feminino'; if (sexo == 'Masculino'){ print('Sexo MASCULINO ai ó');} if (sexo == 'Feminino'){ print('Sexo Feminino ai ó'); } print('finalizou');
