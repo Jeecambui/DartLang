dabb4dc45031bbf3e5f42d6f0febb15a08e6888b
unknown <jeferson_cambui@hotmail.com>
2020-01-19 23:21:00 -0300

    Ultima aula de Funções

--- hello/funcoes.dart
+++ hello/funcoes.dart
@@ -1,9 +1,18 @@
 void main(){
-  boasvindas();
+//  boasvindas();
+  double resSimulator = simulador(56.55, 47.80); 
+  print(resSimulator);
 
+  print(calcArea(2));
 }
 
 void boasvindas(){
   String nome ='Jeferson';
   print ('Bem vindo '+ nome);
 }
+double simulador(double valor1, double valor2){
+  double res = valor1 - valor2;
+  return res;
+}
+
+double calcArea (double raio) => 3.14 * raio * raio;
